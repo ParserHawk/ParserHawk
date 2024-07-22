697cb37168c96e8a78f99f6b9fcf5c496d9fbbbe
Anirudh Sivaraman <sk.anirudh@gmail.com>
2024-07-22 12:03:43 -0400

    Some more tinkering with seq, don't get all of it.

--- sequences.py
+++ sequences.py
@@ -1,7 +1,16 @@
+# use z3 -in to use z3 as a REPL
+
 from z3 import *
 
 s = Solver()
 seq = Const('seq', SeqSort(IntSort()))
 s.add(Length(seq) >= 5)
-x = Int('x')
-print(SubSeq(seq, 0, x))
+s.add(seq.at(0) == Unit(Int(1)))
+s.add(seq.at(1) == Unit(Int(1)))
+s.add(seq.at(2) == Unit(Int(1)))
+s.add(seq.at(3) == Unit(Int(1)))
+s.add(seq.at(4) == Unit(Int(1)))
+#x = Int('x')
+#print(SubSeq(seq, 0, x))
+s.check()
+print(s.model())
