8776ac5f660082a111c3271c9e524b07d1e153f4
Anirudh Sivaraman <sk.anirudh@gmail.com>
2024-07-24 14:26:22 -0400

    Handles io_examples1.txt

--- strings.py
+++ strings.py
@@ -2,12 +2,18 @@ from z3 import *
 
 solver = Solver()
 
-bitstream = String('bitstream')
+bitstream1 = String('bitstream1')
+bitstream2 = String('bitstream2')
 offset = Int('offset')
 
-solver.add(bitstream == "10001001")
-solver.add(SubString(bitstream, 0, offset) == "1000")
-solver.add(SubString(bitstream, offset, Length(bitstream)) == "1001")
+solver.add(bitstream1 == "10001001")
+solver.add(SubString(bitstream1, 0, offset) == "1000")
+solver.add(SubString(bitstream1, offset, Length(bitstream1)) == "1001")
+
+solver.add(bitstream2 == "11001001")
+solver.add(SubString(bitstream2, 0, offset) == "1100")
+solver.add(SubString(bitstream2, offset, Length(bitstream2)) == "1001")
+
 
 print(solver.check())
 print(solver.model())
