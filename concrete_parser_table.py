9a0d49e3ef8979966393924215cf463e1eb6f6e1
Spielerr <kkgstud007@gmail.com>
2024-08-01 11:41:44 -0400

    Adding validate and fixing parser execution code

--- concrete_parser_table.py
+++ concrete_parser_table.py
@@ -13,17 +13,15 @@ class TcamRow:
         self.lookup_val         = t_lookup_val
         self.next_header        = t_next_header
         self.extract_len        = t_extract_len
+
         # same as hdr_len in the Gibb et al. ANCS 2012 paper,
         # refers to how long the current header in bytes is,
         # and hence how much should be deposited into the packet_header_vector and also
         # how much the cursor should be advanced by
-        self.next_lookup_offset = t_next_lookup_offset 
+        self.next_lookup_offset = t_next_lookup_offset
 
     @classmethod
     def create_random_tcam_row(cls):
-        # TODO: Ensure rows are legal, i.e., next_lookup_offsets do not exceed extract_len of the next header.
-        # Verification process would be possible after the entire table is populated, then we can run
-        # a top down verification process, not possible to keep track when creating random tcam rows
 
         # for now, current_header would be sequential 1,2,3...
         # lookup_val is any random hexadecimal number between 0x1 and 0xA (i.e., a 4 bit match pattern)
@@ -31,7 +29,6 @@ class TcamRow:
         # extract_len is any random number between 5 to 10 bytes
         # next_lookup_offset is any random number between 1 to 5 (to ensure legality for the random cases)
         # current granularity is 1 byte at a time.
-
         t_current_header = cls.current_header_counter
         t_lookup_val = hex(random.randint(0x1, 0xA))
         t_next_header = cls.current_header_counter + 1
@@ -54,9 +51,17 @@ class ParserTable:
         for _ in range(self.num_rows):
             self.tcam.append(TcamRow.create_random_tcam_row())
 
-    # Todo: Write a validation procedure to check if a parse table is legal
+    # Ensure rows are legal, i.e., next_lookup_offsets do not exceed extract_len of the next header.
+    # Verification process would be possible after the entire table is populated, then we can run
+    # a top down verification process, not possible to keep track when creating random tcam rows
+    # Write a validation procedure to check if a parse table is legal
     def validate(self):
-        pass
+        present_lookup_offset = self.tcam[0].next_lookup_offset
+        for tcam_row_number in range(1, self.num_rows):
+            if(present_lookup_offset > self.tcam[tcam_row_number].extract_len):
+                return False
+            present_lookup_offset = self.tcam[tcam_row_number].next_lookup_offset
+        return True
 
     # display the parser table as row of dictionaries
     def __str__(self):
@@ -67,6 +72,7 @@ class ParserTable:
 
 
 class Parser:
+
     def __init__(self, t_bytestream):
         self.bytestream = t_bytestream
         self.packet_header_vector = []
@@ -76,40 +82,50 @@ class Parser:
         self.current_row = 0  # to keep track of which tcam row is being processed to finally end the parsing
 
     def execute(self):
-        # TODO: Do a while loop across the bytes in a bytestream until (1) either bytestream is exhausted, (2) failure state is reached, or (3) accept state is reached
-        # Logic for now is if parsing fails to go through all tcam entries, it is a failure, otherwise it succeeds
-        while self.current_row < len(self.parser_table.tcam):
-            if not self.step():
+
+        current_header_to_match = self.parser_table.tcam[0].current_header
+
+        # Do a while loop across the bytes in a bytestream until (1) either bytestream is exhausted, (2) failure state is reached, or (3) accept state is reached
+        while self.cursor < len(self.bytestream):
+            if not self.step(current_header_to_match):
                 print("Parsing failed")
                 return
+            else:
+                self.cursor += self.parser_table.tcam[self.current_row].extract_len
+    
         print("Parsing success")
         print("Packet Header Vector:", self.packet_header_vector)
 
-    def step(self):
-        # TODO: While or for loop across entries in TCAM table
+    def step(self, current_header_to_match):
         row = self.parser_table.tcam[self.current_row]
 
         # Assume extract happens before select, like in Figure 1 of the leapfrog paper.
         # Extract part of the bytestream starting from the current cursor position
         bytestream_segment = self.bytestream[self.cursor : self.cursor + row.extract_len]
+        self.packet_header_vector.append(self.bytestream)
 
         # Extract value from current header to match against the lookup_value
-        to_lookup = bytestream_segment[row.next_lookup_offset: len(row.lookup_val) + row.next_lookup_offset]
-        
         # Match the lookup value with the bytestream portion starting from offset specified 
         # (a portion with same length as lookup_val - due to lack of end offset)
-        if to_lookup == row.lookup_val:
-            self.packet_header_vector.append(bytestream_segment)
-            self.cursor += row.extract_len
-            self.current_row += 1
-            return True
-        else:
-            return False
+        to_lookup = bytestream_segment[row.next_lookup_offset: len(row.lookup_val) + row.next_lookup_offset]
+        
+        # While or for loop across entries in TCAM table
+        for tcam_row_number in range(len(self.parser_table.tcam)):
+            if(self.parser_table.tcam[tcam_row_number].current_header == current_header_to_match):
+                if to_lookup == self.parser_table.tcam[tcam_row_number].lookup_val:
+                    current_header_to_match = next_header
+                    # self.cursor += row.extract_len
+                    self.current_row = tcam_row_number
+                    return True
+        
+        # didn't find any matching lookups in the parse table => parsing fails
+        return False
         
 
 parser_table = ParserTable(5)
 parser_table.create_random_parse_table()
 print(parser_table)
+print(parser_table.validate())
 
-parser = Parser('110101001010110011')
+parser = Parser('0x1ABC97A')
 parser.execute()
