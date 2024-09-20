c8a536567122eeec9ffccfd956c77775293026ab
Spielerr <kkgstud007@gmail.com>
2024-09-19 20:29:48 -0400

    Add first draft of concrete parser in racket

--- rosette/example2.rkt
+++ rosette/example2.rkt
@@ -8,23 +8,32 @@
 ; (sum_vector (list 1 2 3 4))
 
 (define-symbolic e1 e2 integer?)
+(define-symbolic a b integer?)
 
 ; i want to solve for a list of size 2 whose sum is same as the list above
 (define sol
     (solve
         (begin
-            (assume (equal? (sum_vector (list 1 2 3 4)) (sum_vector (list e1 e2))))
+            (assert (equal? (sum_vector (list 1 2 3 4)) (sum_vector (list e1 e2))))
+            ; (assert (equal? (parse (bytestream orig_parse_table)) (parse (bytestream symbolic_parse_table))))
             (assume (> e1 0))
             (assume (> e2 0)))))
 
 sol
 
+(evaluate (+ a b) sol)
+(evaluate (+ e1 e2) sol)
+
 ; but what if I wanted something that's not possible? - sol2 should return unsat
 (define sol2
     (solve
         (begin
-            (assume (equal? (sum_vector (list 1)) (sum_vector (list e1 e2))))
+            (assert (equal? (sum_vector (list 1)) (sum_vector (list e1 e2))))
             (assume (> e1 0))
             (assume (> e2 0)))))
 
 sol2
+
+; Assert: used to define conditions that must be satisfied by the solver, based on which the values can be determined by the solver
+; Assume: used to define the restrictions on the input parameters
+; Both are accumalated in the verification condition (vc) and evaluated in the end
--- rosette/symbolic_interpreter.rkt
+++ rosette/symbolic_interpreter.rkt
@@ -1,13 +1,11 @@
 #lang rosette
-(require racket/control)
-; #lang racket
-; (require racket/list)
 
 ;; Ternary match function that takes two lists of strings/characters and matches them
 (define (ternary-match-and patterns data-list)
   (define (match-helper patterns data-list)
+    
     (cond
-      [(empty? data-list) #t] ; If both are empty, return #t
+      [(vector-empty? data-list) #t] ; If both are empty, return #t
        
       [(not (= (vector-length patterns) (vector-length data-list)))
        #f] ; Return #f if lengths do not match
@@ -31,7 +29,6 @@
                 [(char=? (car p-chars) (car d-chars)) (loop (cdr p-chars) (cdr d-chars))] ; Check if characters match
                 [else #f]))] ; Return #f if a mismatch is found
            ))]))
-      ; ]))
 
   (match-helper patterns data-list))
 
@@ -87,9 +84,9 @@
     (define/public (get-tcam)
       tcam)
     
-    ;; Method to add a TCAM row
-    (define/public (add-tcam-row row)
-      (set! tcam (vector-append tcam (vector row))))
+    ; ;; Method to add a TCAM row
+    ; (define/public (add-tcam-row row)
+    ;   (set! tcam (vector-append tcam (vector row))))
     
     ;; Ensure rows are legal
     ; (define/public (validate)
@@ -119,81 +116,102 @@
 
     (define/public (get-parser-table) parser-table)
 
-
     ;; Step method
-    (define/public (step)
+    (define/public (step current-header-to-match current-lookup-offset phv updated-bytestream) ;parameters to send 
       (define extraction-done #f)
-      (define bytestream-segment "")
-      (define tcam-length (vector-length (send parser-table get-tcam)))
-      (define final-status -1)
-      (printf "length outside loop ~a\n" tcam-length)
+      (define (parser-table-iteration parser-table-tcam packet-header-vector)
+        
+        (define (phv-extraction)
+          (begin (printf "phv: ~a\n" packet-header-vector)
 
-      (for ([tcam-row (+ 1 tcam-length)])
-        (printf "inside step loop: ~a, ~b\n" tcam-row tcam-length)
-        (cond [(equal? tcam-row tcam-length) (begin (printf "exiting\n\n") (set! final-status 0) (break))]
-              [else (begin
-                      (printf "inside else: ~a\n" tcam-row)
-                      (cond [(equal? (send (vector-ref (send parser-table get-tcam) tcam-row) get-current-header) current-header-to-match)
-                            (begin (when (equal? extraction-done #t)
-                                    (when (not (equal? (send (vector-ref (send parser-table get-tcam) tcam-row) get-extract-len) "*"))
-                                    (begin (set! bytestream-segment
-                                                  (substring bytestream
-                                                            cursor
-                                                            (+ cursor (string->number (send (vector-ref (send parser-table get-tcam) tcam-row) get-extract-len)))))
-                                            (set! packet-header-vector (vector-append packet-header-vector (vector bytestream-segment)))
-                                            (set! extraction-done #t))))
-                                  (define to-lookup-all #())
-                                  (for ([lookup-offset-index (vector-length current-lookup-offset)])
-                                    (begin (define to-lookup "*")
-                                            (when (not (equal? (vector-ref current-lookup-offset lookup-offset-index) "*"))
-                                              (begin
-                                                (vector-set! current-lookup-offset lookup-offset-index (string->number (vector-ref current-lookup-offset lookup-offset-index)))
-                                                (set! to-lookup
-                                                      (substring bytestream-segment
-                                                                (vector-ref current-lookup-offset lookup-offset-index)
-                                                                (+ (vector-ref current-lookup-offset lookup-offset-index)
-                                                                    (string-length (vector-ref (send (vector-ref (send parser-table get-tcam) tcam-row) get-lookup-val) lookup-offset-index)))))
-                                                (set! to-lookup-all (vector-append to-lookup-all (vector to-lookup)))
-                                                ))))
-                                  (when (ternary-match-and (send (vector-ref (send parser-table get-tcam) tcam-row) get-lookup-val) to-lookup-all)
-                                  (printf "found match: ~a" tcam-row)
-                                    (begin (if (equal? (send (vector-ref (send parser-table get-tcam) tcam-row) get-next-header) "accept") (begin (set! final-status 2) (break))
-                                                (begin (set! current-header-to-match (send (vector-ref (send parser-table get-tcam) tcam-row) get-next-header))
-                                                      (set! current-lookup-offset (send (vector-ref (send parser-table get-tcam) tcam-row) get-next-lookup-offset))
-                                                      (set! current-row tcam-row) (set! final-status 1) (break)))))
-                                  )]))])))
+          ; TODO: fix this to return phv in all failure cases as well
+          (if (not extraction-done) 
+            (if (not (equal? (send (vector-ref parser-table-tcam 0) get-extract-len) "*"))
+              (begin (set! extraction-done #t) 
+                     (vector-append packet-header-vector 
+                      (vector (substring updated-bytestream 0 
+                        (string->number (send (vector-ref parser-table-tcam 0) get-extract-len)))))) 
+              packet-header-vector) 
+              packet-header-vector)
+        ))
+
+        (define (lookup-extraction)
+          (define (collect-all-offsets lookup-offset-index to-lookup-all)
+            (let* ([bytestream-segment (delay (substring updated-bytestream 0 (string->number (send (vector-ref parser-table-tcam 0) get-extract-len))))]
+                  [to-lookup (delay (substring (force bytestream-segment)
+                              (string->number (vector-ref current-lookup-offset lookup-offset-index))
+                              (+ (string->number (vector-ref current-lookup-offset lookup-offset-index))
+                                  (string-length (vector-ref (send (vector-ref parser-table-tcam 0) get-lookup-val) lookup-offset-index)))))])
+            (if (equal? lookup-offset-index (vector-length current-lookup-offset)) to-lookup-all                       
+            (if (not (equal? (vector-ref current-lookup-offset lookup-offset-index) "*")) 
+              (collect-all-offsets (add1 lookup-offset-index) (vector-append to-lookup-all (vector (force to-lookup))))
+              (collect-all-offsets (add1 lookup-offset-index) to-lookup-all)
+            )))
+          )
+          (collect-all-offsets 0 (vector))
+        )
+
+        (define (process-table)
+          ; if ternary match and accept then return code = 2
+          (printf "~a\n" (send (vector-ref parser-table-tcam 0) get-lookup-val))
+          (if (ternary-match-and (send (vector-ref parser-table-tcam 0) get-lookup-val) (lookup-extraction))
+            (begin (printf "ternary match\n")
+            (if (equal? (send (vector-ref parser-table-tcam 0) get-next-header) "accept")
+              (list 2 (phv-extraction) (send (vector-ref parser-table-tcam 0) get-extract-len) "" (vector))
+              (list 1 (phv-extraction) (send (vector-ref parser-table-tcam 0) get-extract-len) (send (vector-ref parser-table-tcam 0) get-next-header) (send (vector-ref parser-table-tcam 0) get-next-lookup-offset))))
+            (parser-table-iteration (vector-copy parser-table-tcam 1) (phv-extraction))
+            )
+        )
+
+        ; (printf "~a\n" (vector-ref parser-table-tcam 0))
+
+        (if (vector-empty? parser-table-tcam) (list 0 packet-header-vector "" (vector))
+          (if (equal? (send (vector-ref parser-table-tcam 0) get-current-header) current-header-to-match) (process-table) (parser-table-iteration (vector-copy parser-table-tcam 1) packet-header-vector))
+
+        ; (parser-table-iteration (add1 tcam-row-number) parser-table-tcam (phv-extraction))
+        ))
+      (parser-table-iteration (send parser-table get-tcam) phv))
 
     ;; Execute method
-      (define/public (execute)
-        (let my-func ()
-          (when (< cursor (string-length bytestream))
-            (let ([my-status (send this step)])
-            (printf "step return: ~a" my-status)
+      (define/public (execute) ;return (parsing_code, phv)
+        (define (my-func input-bytestream current-header-to-match current-lookup-offset phv)
+          (let ([my-status (delay (step current-header-to-match current-lookup-offset phv input-bytestream))]) ;my-status: (return code, phv, extract-len, current-header-to-match, current-lookup-offset)
+            ; (printf "step return: ~a" my-status)
               (cond
-                [(= my-status 0) 0] ; Parsing error
-                [(= my-status 1)
-                (let* (
-                      [current-row-tcam (vector-ref (send parser-table get-tcam) current-row)]
-                      [curr-length (send current-row-tcam extract-len)]
-                      [int-length (if (equal? curr-length "*") 0 (string->number curr-length))])
-                  (set! cursor (+ cursor int-length))
-                  (my-func))] ; Continue parsing
-                [(= my-status 2) 1])))))
-                                ))
+                [(= (string-length input-bytestream) 0) (list 0 phv)]
+                [(= (list-ref (force my-status) 0) 0) (list 0 (list-ref (force my-status) 1))] ; Parsing error
+                [(and (= (list-ref (force my-status) 0) 1) (not (equal? (list-ref (force my-status) 2) "*")))
+                  (let ([extract-len (string->number (list-ref (force my-status) 2))])
+                    (if (>= extract-len (string-length input-bytestream)) (my-func "" "" (vector) (list-ref (force my-status) 1))
+                    (my-func (substring input-bytestream extract-len) (list-ref (force my-status) 3) (list-ref (force my-status) 4) (list-ref (force my-status) 1))))] ; Continue parsing
+                [(= (list-ref (force my-status) 0) 2) (printf "accepted\n") (list 1 (list-ref (force my-status) 1))])))
+        (my-func bytestream current-header-to-match (vector) (vector)))))      
 
 ;; Function to create the parser table
 (define (create-parser-table1)
-  (let ([parser-table (new parser-table% [tcam #()] [num-rows 9])])
-    ;; Add TCAM rows to the parser table
-    (send parser-table add-tcam-row (new tcam-row% [current-header "S"] [lookup-val #("*")] [next-header "A"] [extract-len "*"] [next-lookup-offset #("0")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "A"] [lookup-val #("*A")] [next-header "B"] [extract-len "3"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "A"] [lookup-val #("*B")] [next-header "C"] [extract-len "3"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "B"] [lookup-val #("*")] [next-header "accept"] [extract-len "1"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "C"] [lookup-val #("*")] [next-header "D"] [extract-len "2"] [next-lookup-offset #("1")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "D"] [lookup-val #("11")] [next-header "E"] [extract-len "4"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "D"] [lookup-val #("01")] [next-header "F"] [extract-len "4"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "E"] [lookup-val #("*")] [next-header "accept"] [extract-len "5"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
-    (send parser-table add-tcam-row (new tcam-row% [current-header "F"] [lookup-val #("*")] [next-header "accept"] [extract-len "6"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+  (let* ([
+          my-tcam (vector 
+          (new tcam-row% [current-header "S"] [lookup-val #("*")] [next-header "A"] [extract-len "0"] [next-lookup-offset #("0")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "A"] [lookup-val #("*A")] [next-header "B"] [extract-len "3"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "A"] [lookup-val #("*B")] [next-header "C"] [extract-len "3"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "B"] [lookup-val #("*")] [next-header "accept"] [extract-len "1"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "C"] [lookup-val #("*")] [next-header "D"] [extract-len "2"] [next-lookup-offset #("1")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "D"] [lookup-val #("11")] [next-header "E"] [extract-len "4"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "D"] [lookup-val #("01")] [next-header "F"] [extract-len "4"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "E"] [lookup-val #("*")] [next-header "accept"] [extract-len "5"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()])
+          (new tcam-row% [current-header "F"] [lookup-val #("*")] [next-header "accept"] [extract-len "6"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+        ]
+        [parser-table (new parser-table% [tcam my-tcam] [num-rows 9])])
+
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "S"] [lookup-val #("*")] [next-header "A"] [extract-len "*"] [next-lookup-offset #("0")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "A"] [lookup-val #("*A")] [next-header "B"] [extract-len "3"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "A"] [lookup-val #("*B")] [next-header "C"] [extract-len "3"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "B"] [lookup-val #("*")] [next-header "accept"] [extract-len "1"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "C"] [lookup-val #("*")] [next-header "D"] [extract-len "2"] [next-lookup-offset #("1")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "D"] [lookup-val #("11")] [next-header "E"] [extract-len "4"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "D"] [lookup-val #("01")] [next-header "F"] [extract-len "4"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "E"] [lookup-val #("*")] [next-header "accept"] [extract-len "5"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
+    ; (send parser-table add-tcam-row (new tcam-row% [current-header "F"] [lookup-val #("*")] [next-header "accept"] [extract-len "6"] [next-lookup-offset #("*")] [lookup-phv-indices #()] [next-lookup-offsets-phv #()]))
     parser-table))
 
 ;; Test parsing function
@@ -213,13 +231,12 @@
 (define (test-phv-1)
   ;; Create the parser table
   (begin
-  (define parser-table (create-parser-table1))
 
   ;; Initialize the parser with the bytestream "2AC1" and the created parser table
   (define parser (new parser%
                       [bytestream "2AC1"]
                       [packet-header-vector #()]
-                      [parser-table parser-table]
+                      [parser-table (create-parser-table1)]
                       [cursor 0]
                       [current-row #()]
                       [current-header-to-match ""]
@@ -227,20 +244,23 @@
 
   (printf "table length:~a\n" (vector-length (send (send parser get-parser-table) get-tcam)))
   ;; Execute the parser
-  (send parser execute)
+  
 
   ;; Get the result, which is the packet header vector
-  (define result (send parser get-packet-header-vector))
+  (define result (send parser execute))
 
   ;; Define the expected value
-  (define expected-value #("2AC" "1"))
+  (define expected-value #("" "2AC" "1"))
 
   ;; Print the result and expected value
-  (printf "result: ~a\n" result)
+  (printf "whole result: ~a\n" result)
+  (printf "result: ~a\n" (list-ref result 1))
+  (printf "first elem: ~a\n" (vector-ref (list-ref result 1) 1))
+  (printf "result len: ~a\n" (length result))
   (printf "expected-value: ~a\n" expected-value)
 
   ;; Assert that the result matches the expected value
-  (equal? result expected-value)))
+  (equal? (list-ref result 1) expected-value)))
 
 ;; Call the test function to run it
 (test-phv-1)
